1ef6f69e3bc31d8995e988d1f61da9422adcb2e2
Throvtek <90162585+Throvtek@users.noreply.github.com>
2023-01-05 13:10:12 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     //const response = await fetch('https://codex-im0y.onrender.com/', {
-    const response = await fetch('https://girl-opener.onrender.com', {
+    const response = await fetch('https://girl-opener5.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
